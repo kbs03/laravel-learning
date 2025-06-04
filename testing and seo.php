2ead8f4b6d0a7284fc71a5aaf87c1e02b07c19cd
kbs03 <131431542+kbs03@users.noreply.github.com>
2025-06-04 16:11:12 +0530

    Add files via upload

---
+++ testing and seo.php
@@ -0,0 +1,43 @@
+Remove Index.html and set the main page URL on the click of the main logo on homepage
+
+remove session data display in dashboard ( for production )
+
+Api collection with proper testing
+
+change folder structure of controller names accordingly
+
+page description view 3 line if clicks on view more than display other data
+
+on select image display only images
+
+Put Proper field validation error
+
+Download online links and place it
+
+Every dropdown with search
+
+corret title and favicon
+
+used common trait & for frontend Apis key authentication implemented and for backend Apis token authentication
+implemented
+
+active menu
+
+in url space replece with -
+
+table created at and updated at
+
+* display in all forms required field
+
+default and alt for image
+
+
+
+Seo related changes ::
+
+
+canonical url
+
+dynamic title and dynamic meta description
+
+changable url
